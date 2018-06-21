03757e23d6e7af79fd698a9e9883098a0c1dfdf6
daniel4w <daniel4w@gmx.de>
2018-06-21 15:57:23 +0200

    change filename

--- TestTravis.php
+++ TestTravis.php
@@ -12,5 +12,9 @@ function run()
 
 	return $site;
 }
-
+$start_time = microtime(true);
 run();
+
+$time_elapsed_secs = microtime(true) - $start_time;
+echo "execution time:    ".$time_elapsed_secs."s\n";
+echo "peak memory usage: ".number_format(memory_get_peak_usage(), 0, ".", ".")." byte\n";
